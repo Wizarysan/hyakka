4f0bec0e23869d176b595cd3b8aa41cbe71df035
Wizary <wizaryn@gmail.com>
2018-12-06 12:33:58 +0300

    Complete boilerplate ver. 1

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Test } from './components/Test';
+declare let module: any
 
-ReactDOM.render(<Test compiler="Typescript" framework="React" bundler="Webpack" />,
+ReactDOM.render(<Test compiler="Typescript" framework="React..." bundler="Webpack" />,
 document.getElementById('root'));
+
+if (module.hot) {
+   module.hot.accept();
+}
