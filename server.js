b4e790f781b1c04a94cea63d2267d1e283a432d6
Wizary <wizaryn@gmail.com>
2018-12-06 13:07:08 +0300

    added opn

--- server.js
+++ server.js
@@ -3,14 +3,21 @@ const path = require('path'),
    webpack = require('webpack'),
    webpackConfig = require('./webpack.config.js'),
    app = express(),
-   port = process.env.PORT || 3000;
+   port = process.env.PORT || 3000,
+   opn = require('opn');
+
 app.listen(port, () => { console.log(`App is listening on port ${port}`) });
+
 app.get('/', (req, res) => {
    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
+
 let compiler = webpack(webpackConfig);
+
 app.use(require('webpack-dev-middleware')(compiler, {
    noInfo: true, publicPath: webpackConfig.output.publicPath, stats:    { colors: true }
 }));
 app.use(require('webpack-hot-middleware')(compiler));
 app.use(express.static(path.resolve(__dirname, 'dist')));
+
+opn('http://localhost:' + port);
