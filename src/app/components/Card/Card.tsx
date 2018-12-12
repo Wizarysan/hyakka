74aa4893b6daed102a1792d3f273a58bf484d2e8
Wizary <wizaryn@gmail.com>
2018-12-12 14:49:34 +0300

    styling card cover

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -8,6 +8,12 @@ interface CardProps {
 
 const CardWrapper = styled.div`
     width: 200px;
+    padding: 5px;
+    transition: all .2s;
+    &:hover {
+        transform: translate(0, -2px);
+        box-shadow: 3px 3px 10px 0px rgba(0,0,0,0.75);
+    }
 `;
 const CardCover = styled.img`
     max-width: 100%;
@@ -16,14 +22,18 @@ const CardTitle = styled.p`
     font-size: 1.2em;
     margin: 5px 0;
 `;
+const StyledLink = styled(Link)`
+    color: black;
+    text-decoration: none;
+`
 
 const Card: React.SFC<CardProps> = (props) => {
     return (
         <CardWrapper className="hyakka-card">
-            <Link to="/fullcard">
+            <StyledLink to="/fullcard">
                 <CardCover src={props.image} />
                 <CardTitle>Test title</CardTitle>
-            </Link>
+            </StyledLink>
         </CardWrapper>
     );
 }
