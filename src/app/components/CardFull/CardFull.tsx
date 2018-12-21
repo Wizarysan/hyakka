afcde59da7895e88c493916234d1d69fd0e57c4e
Wizary <wizaryn@gmail.com>
2018-12-21 18:20:41 +0300

    Full card filtering

--- src/app/components/CardFull/CardFull.tsx
+++ src/app/components/CardFull/CardFull.tsx
@@ -1,17 +1,53 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+import { bindActionCreators } from 'redux';
+import {connect} from 'react-redux';
+import {State} from './../../types/State';
+import {fullCardActions} from './CardFullDuck';
+
 interface FullCardProps {
-    match: any;
+    match: any,
+    results: any, 
+    actions: any,
+    fullCard: any  
 }
 
-export default class CardFull extends React.Component<FullCardProps> {
+class CardFull extends React.Component<FullCardProps> {
+    componentDidMount() {
+        const ownId = parseInt(this.props.match.params.ownId)
+        for(let category in this.props.results.body) {
+            this.props.results.body[category].forEach((item: any)=> {
+                if(item.ownId === ownId) {
+                    this.props.actions.setFullCard(item)
+                }
+            })
+        }
+    }
+    
     render() {
-        console.log(this.props.match)
+        const full = this.props.fullCard;
         return (
             <div className="hyakka-card-full">
-                Full Card {this.props.match.params.ownId}
+                <h1>{full.name}</h1>
+                <img src={full.cover} alt=""/>
             </div>
         )        
     }
 }
+
+function mapStateToProps(state: State) {
+    return {
+      results: state.results,
+      fullCard: state.currentCard
+    };
+  }
+  
+  function mapDispatchToProps(dispatch: any) {
+    return {
+      actions: bindActionCreators<any, any>(fullCardActions, dispatch)
+    }
+  }
+  
+
+export default connect(mapStateToProps, mapDispatchToProps)(CardFull);
