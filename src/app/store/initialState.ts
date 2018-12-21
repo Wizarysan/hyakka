cf877f64fdf1b1dcb9042ee57bb86664640b027f
Wizary <wizaryn@gmail.com>
2018-12-21 12:00:13 +0300

    Making full card state

--- src/app/store/initialState.ts
+++ src/app/store/initialState.ts
@@ -7,5 +7,6 @@ export default {
     },
     filters: {
         name: false
-    }
+    },
+    currentCard: {}
 }
--- src/app/types/State.ts
+++ src/app/types/State.ts
@@ -7,5 +7,20 @@ export type State = {
     },
     filters: {
         name: boolean
+    },
+    currentCard: {
+        id: string,
+        name: string,
+        type: string,
+        format: string,
+        cover: string,
+        synopsis: string,
+        by: string,
+        year: string,
+        genres?: Array<any>,
+        ratings: string,
+        tags: string,
+        ongoing: string,
+        url: string
     }
 }
