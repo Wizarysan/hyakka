7bb1df73de95196eab11088fc615041d367b1ce2
Wizary <wizaryn@gmail.com>
2018-12-25 13:23:05 +0300

    theme helper

---
+++ src/app/helpers/styled-components.ts
@@ -0,0 +1,18 @@
+import * as styledComponents from "styled-components";
+import { ThemedStyledComponentsModule } from "styled-components";
+
+interface ThemeInterface {
+    primaryColor: string;
+    primaryColorInverted: string;
+  }
+
+const {
+  default: styled,
+  css,
+  createGlobalStyle,
+  keyframes,
+  ThemeProvider
+} = styledComponents as ThemedStyledComponentsModule<ThemeInterface>;
+
+export { css, createGlobalStyle, keyframes, ThemeProvider };
+export default styled;
