ccf187193c6723888e034ae143d5d28f6a888ce0
Wizary <wizaryn@gmail.com>
2018-12-27 17:30:15 +0300

    Single entry - Jikan adapter

--- src/app/search/adapters/Jikan.ts
+++ src/app/search/adapters/Jikan.ts
@@ -1,18 +1,40 @@
 //https://jikan.docs.apiary.io
 
 const Jikan = (query: string, type?: string, id?: string) => {
-    const BASE = 'https://api.jikan.moe/v3',
-          SEARCH = `/search/${type}?q=`;
+    const BASE = 'https://api.jikan.moe/v3/',
+          SEARCH = `search/${type}?q=`;
+          
+    let collector = (data: any) => {},
+        call;      
 
     if(id) {
-
+        call = BASE + type + '/' + id; 
+        collector = data => {
+            let ongoing,
+            genres;
+            data.airing!=null ? ongoing = data.airing : ongoing = data.publishing;
+            genres = data.genres.map((genre: any)=> genre.name);
+            return {
+                id: id,
+                ownId: data.mal_id,
+                name: data.title,
+                type: type,
+                format: data.type,
+                cover: data.image_url,
+                synopsis: data.synopsis,
+                by: data.studios[0].name,
+                year: data.premiered,
+                genres: genres,
+                ratings: data.score,
+                tags: '',
+                ongoing: ongoing,
+                url: data.url
+            }
+        }      
     } else {
-        let call = BASE + SEARCH + query;
-        return fetch(call, { method: 'GET'})
-        .then(response=>response.json())
-        .then(data=> {
-            console.log('Jikan, ', data)
-            let results = data.results.map((item: any) => {
+        call = BASE + SEARCH + query;
+        collector = data => {
+            return data.results.map((item: any) => {
                 let ongoing;
                 item.airing!=null ? ongoing = item.airing : ongoing = item.publishing;
                 item.start_date ? item.start_date = item.start_date.substring(0,4) : item.start_date = '';
@@ -32,12 +54,18 @@ const Jikan = (query: string, type?: string, id?: string) => {
                         url: item.url
                     }
                 });
-            return results
-        }).catch(err=> {
-            console.error('Jikan api error ', err);
-            return err;
-        }) 
+        }
     }
+
+    return fetch(call, { method: 'GET'})
+            .then(response=>response.json())
+            .then(data=> {
+                let results = collector(data)
+                return results
+            }).catch(err=> {
+                console.error('Jikan api error ', err);
+                return err;
+            }) 
 }
 
 export default Jikan;
