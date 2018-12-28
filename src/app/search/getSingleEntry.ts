4d92bf28df0a6a12414cf75ca8c0a18f94036dcf
Wizary <wizaryn@gmail.com>
2018-12-28 08:01:12 +0100

    single entry created

---
+++ src/app/search/getSingleEntry.ts
@@ -0,0 +1,9 @@
+import Jikan from "./adapters/Jikan";
+import Vndb from "./adapters/Vndb";
+import Shikimori from "./adapters/Shikimori";
+
+const getSingleEntry = (id: number) => {
+    return id;
+}
+
+export default getSingleEntry
